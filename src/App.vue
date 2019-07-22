38bf717e2209195876328c813315ee001ee1bda1
Danny Zeng <DANNY.ZENG@oocl.com>
2019-07-22 15:37:57 +0800

    vuex counter finish

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Couter></Couter>
+  </div>
+</template>
+
+<script>
+import Couter from './components/Couter'
+
+export default {
+
+  components: {
+    Couter
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Couter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button @click="$store.commit('increment')">+</button>
+    <span>{{$store.state.count}}</span>
+    <button @click="$store.commit('decrement')">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    },
+    methods:{
+        
+    }
+};
+</script>
